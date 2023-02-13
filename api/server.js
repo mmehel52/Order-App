791dbfeaef897c29632675140d59a7756c0a6508
mustafa mehel <mehelx5207@gmail.com>
2023-02-13 19:23:15 +0300

    sdd

--- api/server.js
+++ api/server.js
@@ -1,1 +1,22 @@
-console.log("hello wortttld");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+const app = express();
+const port = 5000;
+
+app.listen(port, () => {
+  connect();
+  console.log(`app is listening on port ${port}`);
+});
