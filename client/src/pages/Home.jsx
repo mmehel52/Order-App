3b2e371e928d04360229719ef22a2668c9c2e368
mustafa mehel <mehelx5207@gmail.com>
2023-02-14 13:20:36 +0300

    aaa

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CartTotals from "../components/CartTotals";
 import Categories from "../components/Categories";
 import Products from "../components/Products";
 import Header from "../components/Header";
 
 const Home = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/categories/get-all");
+        const data = await res.json();
+        setCategories(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCategories();
+  }, []);
+
   return (
     <>
       <Header />
       <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
         <div className="categories  overflow-auto max-h-[calc(100vh112px)] md:pb-10 ">
-          <Categories />
+          <Categories categories={categories} setCategories={setCategories} />
         </div>
         <div className="products flex-[8] overflow-y-auto max-h-[calc(100vh_-_112px)] pb-10">
           <Products />
