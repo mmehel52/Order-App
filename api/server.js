620b18e87596311c8621af7be40ae43c14398ff4
mustafa mehel <mehelx5207@gmail.com>
2023-02-14 12:15:56 +0300

    uu

--- api/server.js
+++ api/server.js
@@ -8,6 +8,7 @@ const port = 5000;
 const categoryRoute = require("./routes/categories.js");
 const productRoute = require("./routes/products.js");
 const billRoute = require("./routes/bills.js");
+const authRoute = require("./routes/auth.js");
 dotenv.config();
 
 const connect = async () => {
@@ -25,6 +26,7 @@ app.use(cors());
 app.use("/api/categories", categoryRoute);
 app.use("/api/products", productRoute);
 app.use("/api/bills", billRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(port, () => {
   connect();
