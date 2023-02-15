e5600dac1c22c49f4e9e9934a891c918df568595
mustafa mehel <mehelx5207@gmail.com>
2023-02-15 16:01:51 +0300

    aaa

--- client/src/components/products/Edit.jsx
+++ client/src/components/products/Edit.jsx
@@ -1,13 +1,12 @@
-import { Button, Form, Input, message, Modal, Table } from "antd";
+import { Button, Form, Input, message, Modal, Select, Table } from "antd";
 import React, { useEffect, useState } from "react";
 
-const Edit = ({
-  isEditModalOpen,
-  setIsEditModalOpen,
-  categories,
-  setCategories,
-}) => {
+const Edit = () => {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [editingItem, setEditingItem] = useState({});
+  const [form] = Form.useForm();
   useEffect(() => {
     const getProducts = async () => {
       try {
@@ -21,19 +20,42 @@ const Edit = ({
 
     getProducts();
   }, []);
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/categories/get-all");
+        const data = await res.json();
+        data &&
+          setCategories(
+            data.map((item) => {
+              return { ...item, value: item.title };
+            })
+          );
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getCategories();
+  }, []);
+
   const onFinish = (values) => {
     try {
-      fetch("http://localhost:5000/api/categories/update-category", {
+      fetch("http://localhost:5000/api/products/update-product", {
         method: "PUT",
-        body: JSON.stringify({ ...values }),
+        body: JSON.stringify({ ...values, productId: editingItem._id }),
         headers: { "Content-type": "application/json; charset=UTF-8" },
       });
-      message.success("Category updated succesfully.");
-      setCategories(
-        categories.map((item) => {
+      message.success("Product updated succesfully.");
+      setProducts(
+        products.map((item) => {
+          if (item._id === editingItem._id) {
+            return values;
+          }
           return item;
         })
       );
+      setIsEditModalOpen(false);
     } catch (error) {
       message.error("Something went to wrong.");
       console.log(error);
@@ -42,13 +64,13 @@ const Edit = ({
   const deleteCategory = (id) => {
     if (window.confirm("Are you sure?")) {
       try {
-        fetch("http://localhost:5000/api/categories/delete-category", {
+        fetch("http://localhost:5000/api/products/delete-product", {
           method: "DELETE",
-          body: JSON.stringify({ categoryId: id }),
+          body: JSON.stringify({ productId: id }),
           headers: { "Content-type": "application/json; charset=UTF-8" },
         });
-        message.success("Category deleted succesfully.");
-        setCategories(categories.filter((item) => item._id !== id));
+        message.success("Product deleted succesfully.");
+        setProducts(products.filter((item) => item._id !== id));
       } catch (error) {
         message.error("Something went to wrong.");
         console.log(error);
@@ -91,18 +113,23 @@ const Edit = ({
       render: (_, record) => {
         return (
           <div>
-            <Button type="link" className="pl-0">
-              Düzenle
+            <Button
+              type="link"
+              className="pl-0"
+              onClick={() => {
+                setIsEditModalOpen(true);
+                setEditingItem(record);
+              }}
+            >
+              Edit
             </Button>
-            <Button type="link" htmlType="submit" className="text-gray-500">
-              Kaydet
-            </Button>
+
             <Button
               type="link"
               danger
               onClick={() => deleteCategory(record._id)}
             >
-              Sil
+              Delete
             </Button>
           </div>
         );
@@ -111,7 +138,7 @@ const Edit = ({
   ];
 
   return (
-    <Form onFinish={onFinish}>
+    <>
       <Table
         bordered
         dataSource={products}
@@ -122,7 +149,69 @@ const Edit = ({
           y: 600,
         }}
       />
-    </Form>
+      <Modal
+        title="Yeni Ürün Ekle"
+        open={isEditModalOpen}
+        onCancel={() => setIsEditModalOpen(false)}
+        footer={false}
+      >
+        <Form
+          layout="vertical"
+          onFinish={onFinish}
+          form={form}
+          initialValues={editingItem}
+        >
+          <Form.Item
+            name="title"
+            label="Product Name"
+            rules={[{ required: true, message: "Product Name is required" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="img"
+            label="Product Image"
+            rules={[{ required: true, message: "Product Images is required" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="price"
+            label="Product Price"
+            rules={[{ required: true, message: "Product Price is required" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="category"
+            label="Choose category"
+            rules={[
+              { required: true, message: "Product Category is required" },
+            ]}
+          >
+            <Select
+              showSearch
+              placeholder="Search to Select"
+              optionFilterProp="children"
+              filterOption={(input, option) =>
+                (option?.title ?? "").includes(input)
+              }
+              filterSort={(optionA, optionB) =>
+                (optionA?.title ?? "")
+                  .toLowerCase()
+                  .localeCompare((optionB?.title ?? "").toLowerCase())
+              }
+              options={categories}
+            />
+          </Form.Item>
+          <Form.Item className="flex justify-end mb-0">
+            <Button type="primary" htmlType="submit">
+              Edit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
   );
 };
 
